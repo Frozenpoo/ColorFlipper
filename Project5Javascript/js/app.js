7401569708c1a65ffb07af163cfb2acf378a7fd9
Chris <59150791+Frozenpoo@users.noreply.github.com>
2021-06-02 14:37:42 -0400

    Add files via upload

---
+++ Project5Javascript/js/app.js
@@ -0,0 +1,15 @@
+const colors = ["green", "#8b0000", "rgba(133,122,200)", "#f15025","#00BFFF","#00FF00","#191970","black"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", function () {
+  const randomNumber = getRandomNumber();
+  // console.log(randomNumber);
+
+  document.body.style.backgroundColor = colors[randomNumber];
+  color.textContent = colors[randomNumber];
+});
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * colors.length);
+}
